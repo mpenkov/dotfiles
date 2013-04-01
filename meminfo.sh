d6090398088b8deab183d461e0629f683d4252ae
Michael Penkov <misha.penkov@gmail.com>
2013-04-01 14:32:09 +0900

    updated .screenrc for solarized-light

--- meminfo.sh
+++ meminfo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 RAM=`cat /proc/meminfo | grep "MemFree" | awk -F" " '{print $2}'`
 SWAP=`cat /proc/meminfo | grep "SwapFree" | awk -F" " '{print $2}'`
-echo -n "${RAM}kb/ram ${SWAP}kb/swap"
+echo -n "${RAM}kb/RAM ${SWAP}kb/swap"
