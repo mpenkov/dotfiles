70e280e6f134b64d22c9e653fbfecef2d812492b
Michael Penkov <m@penkov.dev>
2023-01-15 23:24:48 +0900

    switch layout before locking screen

--- i3/lock-screen.sh
+++ i3/lock-screen.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+#
+# Ensure QWERTY keyboard layout, otherwise we don't be able to unlock the screen
+#
+workswitch -inputlanguage english
 random_image="$(ls -1 ~/Pictures/db/*.png | sort -R | head -n 1)"
 i3lock -i "$random_image" -n
