f7fd61922a43e71a07e3b18eb87f4f823697d086
Michael Penkov <m@penkov.dev>
2023-03-01 10:07:59 +0900

    enable backlight prior to screen lock

--- i3/lock-screen.sh
+++ i3/lock-screen.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #
+# Ensure backlight is not off, otherwise we'll have to unlock while blind.
+# It's a pity the backlight keys don't work while i3lock is running...
+#
+if [ $(xbacklight | cut -d. -f1) == "0" ]; then
+    xbacklight -set 50
+fi
+#
 # Ensure QWERTY keyboard layout, otherwise we don't be able to unlock the screen
 #
 workswitch -inputlanguage english
